d2cb1a02f00db330ee051b20d2cccd4b2a929eec
norbertpilat <norbert.pilat1@gmail.com>
2024-01-28 20:26:09 +0100

    Initial commit

---
+++ src/test/java/com/nbot/newadbot/NewAdBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbot.newadbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAdBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
