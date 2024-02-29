795125f7d46de98417a5e571530871e8583a1e77
norbertpilat <norbert.pilat1@gmail.com>
2024-02-29 18:07:31 +0100

    new branch

--- src/main/java/com/nbot/newadbot/links/LinksRepository.java
+++ src/main/java/com/nbot/newadbot/links/LinksRepository.java
@@ -1,12 +1,8 @@
 package com.nbot.newadbot.links;
 
 import com.nbot.newadbot.user.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
-import java.util.Optional;
 
 
 public interface LinksRepository extends CrudRepository<Links,Long> {
