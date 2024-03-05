e0d7c2e222c65f550bac3a4355718e6c70eb704a
norbertpilat <norbert.pilat1@gmail.com>
2024-03-05 16:50:26 +0100

    new branch

--- src/main/java/com/nbot/newadbot/links/LinksRepository.java
+++ src/main/java/com/nbot/newadbot/links/LinksRepository.java
@@ -1,9 +1,7 @@
 package com.nbot.newadbot.links;
 
 import com.nbot.newadbot.user.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +13,5 @@ public interface LinksRepository extends CrudRepository<Links,Long> {
 
     List<Links> findLinksByUserId(Long userId);
     boolean existsByUser(User user);
+    Optional<Links> findByUser(User user);
 }
--- src/main/java/com/nbot/newadbot/user/UserRepository.java
+++ src/main/java/com/nbot/newadbot/user/UserRepository.java
@@ -1,14 +1,9 @@
 package com.nbot.newadbot.user;
 
-import com.nbot.newadbot.user.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User,Long> {
     Optional<User> getUserByChatId(long chatId);
-    @Query("SELECT u FROM User u")
-    List<User> getAllUsers();
 }
